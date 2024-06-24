8154ccf2366a75a5756f7e4e98fc0bb41aa90301
Morten <morten@kiss.no>
2024-06-24 20:25:04 +0200

    Add Base52 conversion, project setup, and ID generation
    This commit introduces the Base52 conversion functionality allowing numerical values to be converted into Base52 form. In addition, an ID generator is added providing the creation of unique identifiers for the project. Lastly, the commit includes a standard project setup and relevant .gitignore file entries.

---
+++ Boom.Id/Base52.cs
@@ -0,0 +1,23 @@
+﻿namespace Boom.Id;
+
+internal static class Base52
+{
+    private const int BASE = 52; // Matches the number of characters in the allowed characters array below
+    private static readonly char[] BaseChars = "0123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz".ToCharArray();
+
+    public static string ToBase52(this long input)
+    {
+        if (input < 0) throw new ArgumentOutOfRangeException(nameof(input), input, "input cannot be negative");
+
+        if (input == 0) return BaseChars[0].ToString();
+
+        var stack = new Stack<char>();
+        while (input != 0)
+        {
+            stack.Push(BaseChars[input % BASE]);
+            input /= BASE;
+        }
+
+        return new string(stack.ToArray());
+    }
+}
