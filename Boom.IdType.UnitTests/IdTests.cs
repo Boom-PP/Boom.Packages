40d8347ad71341274f6f16db4724e6a5dc054ce9
Morten <morten@kiss.no>
2024-07-14 14:19:58 +0200

    Add FluentAssertions package and extend IdTests
    FluentAssertions package was added to the project to enhance testing capability. Also, the IdTests class was extended with two new methods - "GenerateIds" and "CanImplicitlyConvertToString". As part of these changes, a new static method "FromExisting" was also added to the Id class to generate instances from existing Ids.

--- Boom.IdType.UnitTests/IdTests.cs
+++ Boom.IdType.UnitTests/IdTests.cs
@@ -1,9 +1,25 @@
+using FluentAssertions;
+using Xunit.Abstractions;
+
 namespace Boom.IdType.UnitTests;
 
-public class IdTests
+public class IdTests(ITestOutputHelper testOutputHelper)
 {
-    [Fact]
-    public void PlaceholderTest()
+    [Fact]//(Skip = "Just use for generating some IDs when needed")]
+    public void GenerateIds()
     {
+        for (int i = 0; i < 10; i++)
+            testOutputHelper.WriteLine(Id.NewId());
+    }
+    
+    [Fact]
+    public void CanImplicitlyConvertToString()
+    {
+        var id = Id.NewId();
+
+        string stringId = id;
+
+        stringId.Should().BeOfType<string>(stringId);
+        stringId.Should().Be(id.ToString());
     }
 }
--- Boom.IdType/Id.cs
+++ Boom.IdType/Id.cs
@@ -22,8 +22,17 @@ public sealed record Id
     public bool IsEmpty => string.IsNullOrWhiteSpace(_value);
     
     public static Id Empty => new(string.Empty);
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="Id"/> class from an existing ID value.
+    /// </summary>
+    /// <param name="existingId">The existing ID value to create the instance from.</param>
+    /// <returns>A new instance of the <see cref="Id"/> class.</returns>
+    /// <remarks>NOTE: The existing value is not validated. Use carefully.</remarks>
+    public static Id FromExisting(string existingId) => new(existingId);
+
     public static implicit operator string(Id id) => id.ToString();
-    // public static implicit operator ID(string value) => new(value);
+    // public static implicit operator Id(string value) => new(value);
     public bool Equals(Id? other) => _value == other?._value;
     public override int GetHashCode() => _value?.GetHashCode() ?? 0;
     public override string ToString() => _value ?? string.Empty;
